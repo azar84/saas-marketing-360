c872ef4a94d5cc4011bbdfc3549a6f6fbde8200a
Azar Obeidat <azarmacbook@Mac.gateway.sasktel.net>
2025-07-25 15:41:05 -0600

    Add missing CTA fields to schema and fix TypeScript errors

---
+++ prisma/migrations/20250725214101_add_cta_enhanced_fields/migration.sql
@@ -0,0 +1,11 @@
+-- AlterTable
+ALTER TABLE "ctas" ADD COLUMN     "customId" TEXT,
+ADD COLUMN     "onBlurEvent" TEXT,
+ADD COLUMN     "onClickEvent" TEXT,
+ADD COLUMN     "onFocusEvent" TEXT,
+ADD COLUMN     "onHoverEvent" TEXT,
+ADD COLUMN     "onKeyDownEvent" TEXT,
+ADD COLUMN     "onKeyUpEvent" TEXT,
+ADD COLUMN     "onMouseOutEvent" TEXT,
+ADD COLUMN     "onTouchEndEvent" TEXT,
+ADD COLUMN     "onTouchStartEvent" TEXT;
